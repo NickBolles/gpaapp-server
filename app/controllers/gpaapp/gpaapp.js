e59ba3ebae26c7bf5ad61ca064e6e715e7f305fa
sup3rb0wlz <nickbolles@live.com>
2015-01-08 12:03:12 -0600

    deleted redundant app folder

--- app/controllers/gpaapp/gpaapp.js
+++
@@ -1,9 +0,0 @@
-var mode        = process.env.NODE_ENV || 'development',
-    config      = require('../../config/config.js')[mode],
-    defaultTerm = config.defaultTerm,
-    mongoose    = require('mongoose'),
-    User        = mongoose.model('User'),
-    GpaappData  = mongoose.model('GpaappData');
-
-module.exports.controller = require('./controller');
-module.exports.router = require('./router');
